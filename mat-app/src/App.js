7f82c2073830df87583edcd29b24072d9abbefde
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-05-02 15:34:54 -0500

    WIP

--- mat-app/src/App.js
+++ mat-app/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Container from '@material-ui/core/Container';
 
 
 function App() {
   return (
     <div className="App">
-      <Container maxWidth="sm" />
       Hello
     </div>
   );
